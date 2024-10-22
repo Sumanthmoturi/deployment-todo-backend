b82e420e5b8ce9d08ea8058712d4564840df8cbd
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-10-22 16:44:19 +0530

    first commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,40 +1,22 @@
-/*
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { User } from './user.entity';
-
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: 'your_jwt_secret',
-      signOptions: { expiresIn: '1d' },
-    }),
-  ],
-  providers: [AuthService],
-  controllers: [AuthController],
-})
-export class AuthModule {}
-
-*/
-
-// auth.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]), // Register the User entity with TypeORM
-    JwtModule.register({
-      secret: 'your_jwt_secret', // Replace with a secure secret
-      signOptions: { expiresIn: '60s' }, // Token expiration time
+    ConfigModule, // Import ConfigModule to access environment variables
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // Get JWT secret from environment variable
+        signOptions: { expiresIn: '60s' }, // Adjust token expiration time as needed
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
