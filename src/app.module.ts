749ec00716b264d25300a4e034604ccc5840493b
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-10-22 15:47:57 +0530

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,17 +30,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { TodoModule } from './todos/todos.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    // Loads environment variables from a .env file
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+  
     TypeOrmModule.forRoot({
       type: 'postgres',  // Use PostgreSQL as the database
       host: 'localhost', // Database host
       port: 5432,        // Default PostgreSQL port
       username: 'postgres', // Your PostgreSQL username
       password: 'new_password', // Your PostgreSQL password
-      database: 'postgres', // Database name to connect to
+      database: 'postgres', 
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true, // Automatically load entities
       synchronize: true, // Synchronize the database schema with entities (only for development)
     }),
