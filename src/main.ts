f685e1169be062075501e7fe3ec12bce4dcb6d27
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-16 19:11:07 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,21 @@ import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exception.filter';
 import { MyLoggerService } from './my-logger/my-logger.service';
-import { NestExpressApplication } from '@nestjs/platform-express';
-
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  const app = await NestFactory.create(AppModule, {
     bufferLogs:true,
   })
-  const logger = app.get(MyLoggerService);
+  const logger = app.get(MyLoggerService); // Get your logger service instance
   app.useLogger(logger);
   const {httpAdapter} =app.get(HttpAdapterHost)
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
   
   app.enableCors({
-    origin: 'https://todosapp-bay.vercel.app/',
+    origin: 'http://localhost:3002',
     credentials: true,
-
   });
-  
-  await app.init();
-  return app.getHttpServer();
 
- 
+  await app.listen(process.env.PORT || 3001);
+  logger.log('Backend is running on http://localhost:3001');
 }
-bootstrap().then(() => console.log('NestJS app running in serverless mode.'));
+bootstrap();
