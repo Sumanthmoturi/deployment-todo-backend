b3d0d74428bbc33785a9eea3882f5af2276bf78d
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-16 21:06:39 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,15 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
   
   app.enableCors({
-    origin: 'http://localhost:3002',
+    origin: [ 'https://todosapp-msks-projects-c94f807c.vercel.app/',
+             'http://localhost:3002',
+    ],
     credentials: true,
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
 
   await app.listen(process.env.PORT || 3001);
-  logger.log('Backend is running on http://localhost:3001');
+  logger.log('Backend is running on https://deployment-todo-backend.onrender.com');
 }
 bootstrap();
