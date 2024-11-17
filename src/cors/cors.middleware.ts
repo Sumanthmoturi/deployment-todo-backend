d4d41ae5c1a0a655448e689df481304da903104e
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-17 21:26:42 +0530

    commit

--- src/cors/cors.middleware.ts
+++ src/cors/cors.middleware.ts
@@ -1,17 +1,13 @@
-// src/cors.middleware.ts
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    
-    res.header('Access-Control-Allow-Origin', 'https://todosapp-kappa-nine.vercel.app'); // Replace with your frontend URL
+    res.header('Access-Control-Allow-Origin', 'https://todosapp-kappa-nine.vercel.app/'); 
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
-
-    
     if (req.method === 'OPTIONS') {
       return res.status(200).end();
     }
