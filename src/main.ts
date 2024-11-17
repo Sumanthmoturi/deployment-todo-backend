bb682d61c0f3886509a2962cfebe05dea88593d0
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-17 19:26:03 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,8 @@ async function bootstrap() {
   app.enableCors({
     origin:  'https://todosapp-kappa-nine.vercel.app/',
     credentials: true,
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    methods:'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    allowedHeaders:['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT || 3001);
