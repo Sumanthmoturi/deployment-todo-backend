1602af47deeb2593916b3ea2fdb3077e54cba5c4
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-11-22 17:28:18 +0530

    commit

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './todo.entity';
 import { TodoService } from './todos.service';
 import { TodoController } from './todos.controller';
+import { MyLoggerService } from '../my-logger/my-logger.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo])], 
-  providers: [TodoService],
+  providers: [TodoService, MyLoggerService],
   controllers: [TodoController],
 })
 export class TodoModule {}
