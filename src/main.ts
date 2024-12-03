f96bb83e7a1f535206034835009b91fc4ccc2b02
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-03 13:43:44 +0530

    deploy

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
 );
 
   app.enableCors({
-    origin: 'http://13.201.192.253:3002',
+    origin: 'http://65.1.106.158:3002',
     credentials: true, 
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
