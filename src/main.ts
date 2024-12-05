35247e47e91e2aa352937f176d8289bfaac9fb78
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-05 19:31:41 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs:true,
   })
-  
+
   const logger = app.get(MyLoggerService);
   app.useLogger(logger);
   const {httpAdapter} =app.get(HttpAdapterHost)
@@ -32,7 +32,7 @@ async function bootstrap() {
 );
 
   app.enableCors({
-    origin: 'http://ec2-13-201-22-238.ap-south-1.compute.amazonaws.com:3002',
+    origin: 'http://ec2-13-201-22-238.ap-south-1.compute.amazonaws.com',
     credentials: true, 
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -42,7 +42,7 @@ async function bootstrap() {
   const port = process.env.PORT || 10000;
   await app.listen(port, '0.0.0.0');
   
-  logger.log(`Backend is running on http://localhost:${port} or on Render at http://ec2-13-201-101-208.ap-south-1.compute.amazonaws.com:10000`);
+  logger.log(`Backend is running on http://localhost:${port} or on Render at http://ec2-13-201-101-208.ap-south-1.compute.amazonaws.com`);
 }
 bootstrap();
 
