3924f4c9e70e5d410631b90326882b26160b6b5d
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-18 11:58:33 +0530

    token issue

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -42,7 +42,7 @@ export class AuthController {
       
       res.cookie('access_token', accessToken, {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: 'none',
         maxAge: 7* 24* 60* 60*1000, 
       });
--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,10 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: any) {
-   
-    return super.canActivate(context);
+  constructor(private jwtService: JwtService) {
+    super();
+  }
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+
+  async canActivate(context) {
+    const request = context.switchToHttp().getRequest();
+    const token = request.cookies['access_token'];
+    
+    if (!token) {
+      throw new UnauthorizedException('Token missing');
+    }
+
+    try {
+      const user = await this.jwtService.verifyAsync(token);
+      request.user = user; 
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
   }
 }
