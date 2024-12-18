a5b1fc264c52fc9b6b73fb2373c065768eabedef
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-18 14:16:08 +0530

    parseint

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,21 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { AuthService } from './auth.service';
-import { User } from '../user/user.entity';
+import { Request } from 'express';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private readonly authService: AuthService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        const token = req.cookies['access_token'];
+        return token ? token : null;
+      },
       secretOrKey: process.env.JWT_SECRET,
     });
   }
 
   async validate(payload: any): Promise<any> {
-   
-    
     return { userId: Number(payload.sub) }; 
   }
 }
