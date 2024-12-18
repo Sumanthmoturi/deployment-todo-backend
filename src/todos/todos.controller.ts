c266a910ae590bd3d1264b3e249cd4f2f4f6600b
Sumanthmoturi <sumanthmoturi001@gmail.com>
2024-12-18 13:01:33 +0530

    parseint

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -9,29 +9,29 @@ export class TodoController {
   constructor(private todoService: TodoService) {}
   
   @Get()
-  async findAll(@Query('userId') userId: number,@Query('status') status?: 'In progress' | 'Completed'): Promise<Todo[]> {
+  async findAll(@Query('userId', ParseIntPipe) userId: number,@Query('status') status?: 'In progress' | 'Completed'): Promise<Todo[]> {
     return this.todoService.findAll(userId, status);
   }
 
   @Post()
-  async create(@Body() createTodoDto: CreateTodoDto, @Query('userId') userId: number): Promise<Todo> {
+  async create(@Body() createTodoDto: CreateTodoDto, @Query('userId',ParseIntPipe) userId: number): Promise<Todo> {
      return this.todoService.create(createTodoDto, userId);
   }
 
 
   @Patch(':id/status')
-  async updateStatus(@Param('id',ParseIntPipe) id: number, @Body() body: UpdateTodoStatusDto, @Query('userId') userId: number,): Promise<Todo> {
+  async updateStatus(@Param('id',ParseIntPipe) id: number, @Body() body: UpdateTodoStatusDto, @Query('userId',ParseIntPipe) userId: number,): Promise<Todo> {
     return this.todoService.updateStatus(id, body.status, userId);
   }
 
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number, @Query('userId') userId: number,): Promise<{ message: string }> {
+  async delete(@Param('id', ParseIntPipe) id: number, @Query('userId',ParseIntPipe) userId: number,): Promise<{ message: string }> {
     await this.todoService.remove(id, userId);
     return { message: `Todo with ID ${id} has been deleted successfully.` };
   }
 
   @Get(':id')
-  async findOne(@Param('id',ParseIntPipe) id: number, @Query('userId') userId: number,): Promise<Todo> {
+  async findOne(@Param('id',ParseIntPipe) id: number, @Query('userId',ParseIntPipe) userId: number,): Promise<Todo> {
     return this.todoService.findOne(id, userId);
   }
 }
